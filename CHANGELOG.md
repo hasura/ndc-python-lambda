496f92e811678576206c605c297f682967e55106
Daniel Harvey <danieljamesharvey@gmail.com>
2025-08-18 12:04:59 +0100

    Fix examples to use new package name (#8)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,9 @@ This changelog documents the changes between release versions.
 ## [Unreleased]
 Changes to be included in the next upcoming release
 
+## 0.2.2 - 2025.08.18
+* Fix templates to refer to new package
+
 ## [0.2.1] - 2025.08.15
 * Use `ndc-sdk-python` package version `v0.40`
 
--- connector-definition/template/functions.py
+++ connector-definition/template/functions.py
@@ -7,13 +7,13 @@ When you add a Python Lambda connector to your Hasura project, this file is gene
 In this file you'll find code examples that will help you get up to speed with the usage of the Hasura lambda connector.
 If you are an old pro and already know what is going on you can get rid of these example functions and start writing your own code.
 """
-from hasura_ndc import start
-from hasura_ndc.instrumentation import with_active_span # If you aren't planning on adding additional tracing spans, you don't need this!
+from ndc_sdk_python import start
+from ndc_sdk_python.instrumentation import with_active_span # If you aren't planning on adding additional tracing spans, you don't need this!
 from opentelemetry.trace import get_tracer # If you aren't planning on adding additional tracing spans, you don't need this either!
-from hasura_ndc.function_connector import FunctionConnector
+from ndc_sdk_python.function_connector import FunctionConnector
 from pydantic import BaseModel, Field # You only need this import if you plan to have complex inputs/outputs, which function similar to how frameworks like FastAPI do
 import asyncio # You might not need this import if you aren't doing asynchronous work
-from hasura_ndc.errors import UnprocessableContent
+from ndc_sdk_python.errors import UnprocessableContent
 from typing import Annotated
 
 connector = FunctionConnector()
